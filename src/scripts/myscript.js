2b23c5ea4bc5764a96b8ce8725d748f37e8929f1
anjoalo1@gmail.com <anjojalo1@gmail.com>
2023-09-05 17:03:17 -0500

    proyectocss-astro

---
+++ src/scripts/myscript.js
@@ -0,0 +1,24 @@
+import Prism from 'prismjs';
+
+// Import PrismJS extensions
+import 'prism-themes/themes/prism-material-dark.css';
+/* import 'prismjs/components/prism-scss';*/
+import 'prismjs/components/prism-css';
+
+
+// Import Prism JS
+
+import 'prismjs/plugins/custom-class/prism-custom-class';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
+
+
+
+// Highlight all matching syntax
+
+/* Prism.highlightAll(); */
+
+
+
+
