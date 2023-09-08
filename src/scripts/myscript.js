dde7ea07e20b1366ab23f038f14d6693c08a2706
anjoalo1@gmail.com <anjojalo1@gmail.com>
2023-09-08 18:19:06 -0500

    finalizando-especificidad

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,24 +1,8 @@
 import Prism from 'prismjs';
-
-// Import PrismJS extensions
 import 'prism-themes/themes/prism-material-dark.css';
-/* import 'prismjs/components/prism-scss';*/
 import 'prismjs/components/prism-css';
-
-
-// Import Prism JS
-
 import 'prismjs/plugins/custom-class/prism-custom-class';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
-
-
-
-// Highlight all matching syntax
-
-/* Prism.highlightAll(); */
-
-
-
-
+Prism.highlightAll();
