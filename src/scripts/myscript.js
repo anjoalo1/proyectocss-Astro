0741e7403295f9f361637c88ef33d19827de8b48
anjoalo1 <anjojalo1@gmail.com>
2026-03-25 19:17:03 -0500

    curso cambios_25_03_2026

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,8 +1,19 @@
 import Prism from 'prismjs';
-import 'prism-themes/themes/prism-material-dark.css';
+import 'prism-theme-one-dark/prism-onedark.css';
 import 'prismjs/components/prism-css';
 import 'prismjs/plugins/custom-class/prism-custom-class';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/toolbar/prism-toolbar'
+import 'prismjs/plugins/toolbar/prism-toolbar.css'
+import 'prismjs/plugins/show-language/prism-show-language'
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
+import 'prismjs/plugins/inline-color/prism-inline-color'
+import 'prismjs/plugins/inline-color/prism-inline-color.css'
+//match braces resaltar parentesis llaves
+import 'prismjs/plugins/match-braces/prism-match-braces'
+import 'prismjs/plugins/match-braces/prism-match-braces.css'
+import 'prismjs/plugins/previewers/prism-previewers'
+import 'prismjs/plugins/previewers/prism-previewers.css'
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
 Prism.highlightAll();
